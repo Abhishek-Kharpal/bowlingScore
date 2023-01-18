192a4e12451cfcef35c78645bf1cf0e2b32571b1
Abhishek <abhikharpal@gmail.com>
2023-01-18 13:02:38 +0530

    chore: formatting

--- bowlingScore.js
+++ bowlingScore.js
@@ -1,169 +1,169 @@
 let validFrame = (rolls) =>{
-  if(!Array.isArray(rolls)){
-    throw new Error('Rolls not an array');
-  }
-  rolls.forEach((element)=>{
-    if(typeof element !== 'number'){
-      throw new Error('Invalid Frame');
-    }
-  })
-}
+	if(!Array.isArray(rolls)){
+		throw new Error('Rolls not an array');
+	}
+	rolls.forEach((element)=>{
+		if(typeof element !== 'number'){
+			throw new Error('Invalid Frame');
+		}
+	});
+};
 
 let score = (rolls) =>{
 
-    validFrame(rolls);
-    let result = 0;
-    let currIndex = 0;
+	validFrame(rolls);
+	let result = 0;
+	let currIndex = 0;
     
-    for(let i=0;i<=9;i++)
-    {
+	for(let i=0;i<=9;i++)
+	{
 
-      // console.log(result+" "+currIndex+" "+i+"\n")
-      let moveIndex=currIndex;
+		// console.log(result+" "+currIndex+" "+i+"\n")
+		let moveIndex=currIndex;
 
-      if(rolls[moveIndex]===10){
-        while((moveIndex<rolls.length)&&(rolls[moveIndex]===10)){
-          result+=10;
-          ++moveIndex;
-        }
-        if((i==9)&&(moveIndex===rolls.length)){
-          break;
-        }
-        if((moveIndex+1<rolls.length)){
-          result+=rolls[moveIndex];
-          ++moveIndex;
-          result+=rolls[moveIndex];
-        }
-        else{
-          throw new Error('Rolls contains more than 10 frames');
-        }
-        ++currIndex;
-        if((i==9)&&(moveIndex!==rolls.length)){
-          throw new Error('Rolls contains more than 10 frames');
-        }
-        continue;
-      }
+		if(rolls[moveIndex]===10){
+			while((moveIndex<rolls.length)&&(rolls[moveIndex]===10)){
+				result+=10;
+				++moveIndex;
+			}
+			if((i==9)&&(moveIndex===rolls.length)){
+				break;
+			}
+			if((moveIndex+1<rolls.length)){
+				result+=rolls[moveIndex];
+				++moveIndex;
+				result+=rolls[moveIndex];
+			}
+			else{
+				throw new Error('Rolls contains more than 10 frames');
+			}
+			++currIndex;
+			if((i==9)&&(moveIndex!==rolls.length)){
+				throw new Error('Rolls contains more than 10 frames');
+			}
+			continue;
+		}
       
 
-      //Case for spare
+		//Case for spare
 
-      if(moveIndex+1<rolls.length){
-        if(rolls[moveIndex]+rolls[moveIndex+1]===10){
-          result+=10;
-          currIndex+=2;
-          moveIndex+=2;
-          if(moveIndex<rolls.length){
-            result+=rolls[moveIndex];
-          }
-          else{
-            throw new Error('Rolls contains more than 10 frames');
-          }
-        }
-        else{
-          result+=rolls[moveIndex]+rolls[moveIndex+1];
-          currIndex+=2;
-        }
-      }
-      else{
-        throw new Error('Rolls contains more than 10 frames');
-      }
+		if(moveIndex+1<rolls.length){
+			if(rolls[moveIndex]+rolls[moveIndex+1]===10){
+				result+=10;
+				currIndex+=2;
+				moveIndex+=2;
+				if(moveIndex<rolls.length){
+					result+=rolls[moveIndex];
+				}
+				else{
+					throw new Error('Rolls contains more than 10 frames');
+				}
+			}
+			else{
+				result+=rolls[moveIndex]+rolls[moveIndex+1];
+				currIndex+=2;
+			}
+		}
+		else{
+			throw new Error('Rolls contains more than 10 frames');
+		}
 
-      if((i==9)&&(currIndex!==rolls.length)){
-        throw new Error('Rolls contains more than 10 frames');
-      }
-    }
-    return result;
+		if((i==9)&&(currIndex!==rolls.length)){
+			throw new Error('Rolls contains more than 10 frames');
+		}
+	}
+	return result;
 
-}
+};
 
 let bestScore = (rolls) =>{
-  validFrame(rolls);
-    let result = 0;
-    let currIndex = 0;
-    let maxRes = 0;
-    let prevRes = 0;
-    for(let i=0;i<=9;i++)
-    {
-      let moveIndex=currIndex;
-      if(i>=1){
-        if((result-prevRes)>maxRes){
-          maxRes=(result-prevRes)
-        }
-        prevRes=result;
-      }
-      if(rolls[moveIndex]===10){
-        while((moveIndex<rolls.length)&&(rolls[moveIndex]===10)){
-          result+=10;
-          ++moveIndex;
-        }
-        if((i==9)&&(moveIndex===rolls.length)){
-          break;
-        }
-        if((moveIndex+1<rolls.length)){
-          result+=rolls[moveIndex];
-          ++moveIndex;
-          result+=rolls[moveIndex];
-        }
-        else{
-          throw new Error('Rolls contains more than 10 frames');
-        }
-        ++currIndex;
-        if(i>=1){
-          if((result-prevRes)>maxRes){
-            maxRes=(result-prevRes)
-          }
-          prevRes=result;
-        }
-        if((i==9)&&(moveIndex!==rolls.length)){
-          throw new Error('Rolls contains more than 10 frames');
-        }
-        continue;
-      }
+	validFrame(rolls);
+	let result = 0;
+	let currIndex = 0;
+	let maxRes = 0;
+	let prevRes = 0;
+	for(let i=0;i<=9;i++)
+	{
+		let moveIndex=currIndex;
+		if(i>=1){
+			if((result-prevRes)>maxRes){
+				maxRes=(result-prevRes);
+			}
+			prevRes=result;
+		}
+		if(rolls[moveIndex]===10){
+			while((moveIndex<rolls.length)&&(rolls[moveIndex]===10)){
+				result+=10;
+				++moveIndex;
+			}
+			if((i==9)&&(moveIndex===rolls.length)){
+				break;
+			}
+			if((moveIndex+1<rolls.length)){
+				result+=rolls[moveIndex];
+				++moveIndex;
+				result+=rolls[moveIndex];
+			}
+			else{
+				throw new Error('Rolls contains more than 10 frames');
+			}
+			++currIndex;
+			if(i>=1){
+				if((result-prevRes)>maxRes){
+					maxRes=(result-prevRes);
+				}
+				prevRes=result;
+			}
+			if((i==9)&&(moveIndex!==rolls.length)){
+				throw new Error('Rolls contains more than 10 frames');
+			}
+			continue;
+		}
       
 
-      //Case for spare
+		//Case for spare
 
-      if(moveIndex+1<rolls.length){
-        if(rolls[moveIndex]+rolls[moveIndex+1]===10){
-          result+=10;
-          currIndex+=2;
-          moveIndex+=2;
-          if(moveIndex<rolls.length){
-            result+=rolls[moveIndex];
-          }
-          if(i>=1){
-            if((result-prevRes)>maxRes){
-              maxRes=(result-prevRes)
-            }
-            prevRes=result;
-          }
-          else{
-            throw new Error('Rolls contains more than 10 frames');
-          }
-        }
-        else{
-          result+=rolls[moveIndex]+rolls[moveIndex+1];
-          currIndex+=2;
-          if(i>=1){
-            if((result-prevRes)>maxRes){
-              maxRes=(result-prevRes)
-            }
-            prevRes=result;
-          }
-        }
-      }
-      else{
-        throw new Error('Rolls contains more than 10 frames');
-      }
+		if(moveIndex+1<rolls.length){
+			if(rolls[moveIndex]+rolls[moveIndex+1]===10){
+				result+=10;
+				currIndex+=2;
+				moveIndex+=2;
+				if(moveIndex<rolls.length){
+					result+=rolls[moveIndex];
+				}
+				if(i>=1){
+					if((result-prevRes)>maxRes){
+						maxRes=(result-prevRes);
+					}
+					prevRes=result;
+				}
+				else{
+					throw new Error('Rolls contains more than 10 frames');
+				}
+			}
+			else{
+				result+=rolls[moveIndex]+rolls[moveIndex+1];
+				currIndex+=2;
+				if(i>=1){
+					if((result-prevRes)>maxRes){
+						maxRes=(result-prevRes);
+					}
+					prevRes=result;
+				}
+			}
+		}
+		else{
+			throw new Error('Rolls contains more than 10 frames');
+		}
 
-      if((i==9)&&(currIndex!==rolls.length)){
-        throw new Error('Rolls contains more than 10 frames');
-      }
-    }
-    return maxRes;
-}
+		if((i==9)&&(currIndex!==rolls.length)){
+			throw new Error('Rolls contains more than 10 frames');
+		}
+	}
+	return maxRes;
+};
 
 
 module.exports={
-  bestScore,score 
-}
+	bestScore,score 
+};
--- bowlingScore.test.js
+++ bowlingScore.test.js
@@ -1,38 +1,47 @@
-const {bestScore,score} = require('./bowlingScore')
+const {bestScore,score} = require('./bowlingScore');
 
 
 describe('bowlingScore', () => {
-  describe('score', () => {
+	describe('score', () => {
 
-    it('should return score of game', () => {
-      expect(score([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
-    });
+		it('should return score of game', () => {
+			expect(score([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(90);
+		});
 
-    it('should throw error when frame is not a number',()=>{
-      expect(()=>score(['string'])).toThrow('Invalid Frame');
-    });
+        it('should return score of game', () => {
+			expect(score([6, 4, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])).toBe(16);
+		});
 
-    it('should throw error when roll is not an array',()=>{
-      expect(()=>score(243)).toThrow('Rolls not an array');
-    });
+        it('should return score of game', () => {
+			expect(score([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10])).toBe(30);
+		});
 
-  });
+		it('should throw error when frame is not a number',()=>{
+			expect(()=>score(['string'])).toThrow('Invalid Frame');
+		});
+
+		it('should throw error when roll is not an array',()=>{
+			expect(()=>score(243)).toThrow('Rolls not an array');
+		});
+
+
+	});
 });
 
 
 describe('bowlingScore', () => {
-    describe('bestScore', () => {
+	describe('bestScore', () => {
   
-      it('should return score of game', () => {
-        expect(bestScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(9);
-      });
-      it('should throw error when frame is not a number',()=>{
-        expect(()=>score(['string'])).toThrow('Invalid Frame');
-      });
+		it('should return score of game', () => {
+			expect(bestScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6])).toBe(9);
+		});
+		it('should throw error when frame is not a number',()=>{
+			expect(()=>score(['string'])).toThrow('Invalid Frame');
+		});
   
-      it('should throw error when roll is not an array',()=>{
-        expect(()=>score(243)).toThrow('Rolls not an array');
-      });
+		it('should throw error when roll is not an array',()=>{
+			expect(()=>score(243)).toThrow('Rolls not an array');
+		});
   
-    });
-  });
+	});
+});
